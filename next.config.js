cbf870f9014f5fb25f50751b3ed12f36e844b6c3
Yuen Ler Chow <lerchow@gmail.com>
2024-09-15 09:59:29 -0400

    builds

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ await import("./env.mjs");
 
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint during production builds
+  },
 };
 
 export default nextConfig;
